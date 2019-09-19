e2b437567a1613ba4ab4b3269c6ed7766e10f76f
rusktk@gmail.com <Ruslan.Gatiyatullin@icl-services.com>
2019-09-19 18:00:02 +0300

    adding comment header

--- connect-EOL.ps1
+++ connect-EOL.ps1
@@ -1,3 +1,45 @@
+#requires -version 2
+<#
+.SYNOPSIS
+  The script connects Exchange Online
+.DESCRIPTION
+  <Brief description of script>
+.PARAMETER
+    <Parameter_Name>
+        <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+.NOTES
+  Version:        1.0
+  Author:         Ruslan Gatiyatullin
+  Creation Date:  19.09.2019
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+#$ErrorActionPreference = "SilentlyContinue"
+
+#Dot Source required Function Libraries
+#. "C:\Scripts\Functions\Logging_Functions.ps1"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+
+
+#Log File Info
+#$sLogPath = "C:\Windows\Temp"
+#$sLogName = "<script_name>.log"
+#$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
 function Connect-EOL {
     param (
         #$UserPrincipalName,
@@ -22,3 +64,10 @@ function Connect-EOL {
         return $true
     }
 }
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
+#Script Execution goes here
+#Log-Finish -LogPath $sLogFile
+
