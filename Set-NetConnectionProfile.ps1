9798f4861e405560f484b8f6e7933fb5a1984398
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2019-09-26 10:59:50 +0300

    tool to keep VPN connection in private profile

---
+++ Set-NetConnectionProfile.ps1
@@ -0,0 +1,81 @@
+<#
+#### requires ps-version 3.0 ####
+<#
+.SYNOPSIS
+	<The script prevents changing connection profile via NLA service>
+.DESCRIPTION
+<Brief description of script>
+.PARAMETER <Parameter_Name>
+<Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+<Inputs if any, otherwise state None>
+.OUTPUTS
+<Outputs if anything is generated>
+.NOTES
+   Version:        0.1
+   Author:         Ruslan Gatiyatullin
+   Creation Date:  Thursday, September 26th 2019, 9:55:21 am
+   File: Set-NetConnectionProfile.ps1
+   Copyright (c) 2019 <<company>>
+HISTORY:
+Date      	          By	Comments
+----------	          ---	----------------------------------------------------------
+
+.LINK
+   <<website>>
+
+.COMPONENT
+ Required Modules: 
+
+.LICENSE
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the Software), to deal
+in the Software without restriction, including without limitation the rights
+to use copy, modify, merge, publish, distribute sublicense and /or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ 
+.EXAMPLE
+<Example goes here. Repeat this attribute for more than one example>
+#
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action 
+#$ErrorActionPreference = 'SilentlyContinue'
+#---------------------------------------------------------[Variables]--------------------------------------------------------
+#Log File Info
+#$sLogPath = 
+#$sLogName = script_name.log
+#$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#---------------------------------------------------------[Functions]--------------------------------------------------------
+
+#---------------------------------------------------------[Main]--------------------------------------------------------
+#>
+
+#eternal loop
+while ($true -ne $false) {
+
+	$NetworkProfile = Get-NetConnectionProfile -InterfaceAlias "Ethernet 6"
+	if ($NetworkProfile.NetworkCategory -eq "Public") {
+		Write-Host "switching to private"
+		#$NetworkProfile.NetworkCategory = "Private"
+		#Set-NetConnectionProfile -InputObject $NetworkProfile
+		#Restart-Service -Name "Network Location Awareness" -Force
+		Set-NetConnectionProfile -InterfaceAlias "Ethernet 6" -NetworkCategory "Private"
+	}
+	Remove-Variable NetworkProfile
+	Start-Sleep 1
+}
